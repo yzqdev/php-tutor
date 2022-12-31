6d4d304ed2dc8536994235ebaba47f51bfce2309
yzqdev <yzqdev@outlook.com>
2022-12-31 16:24:41 +0800

    init

---
+++ php-learn/index.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<?php
+$file = fopen("welcome.txt", "r") or exit("无法打开文件!");
+// 读取文件每一行，直到文件结尾
+while(!feof($file))
+
+{
+    echo fgets($file). "<br>";
+
+}
+fclose($file);
+?>
+
+</body>
+</html>
